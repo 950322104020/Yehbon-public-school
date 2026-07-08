daae7af26bb7bd54ae856752c37ea55c3c9dde28
Arun Saini <shyamzacx@gmail.com>
2026-07-08 22:55:15 +0530

    Update public-frontend/utils/api.js

--- public-frontend/utils/api.js
+++ public-frontend/utils/api.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
-
-const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
-});
-
-// Add these two lines
-console.log("NEXT_PUBLIC_API_URL:", process.env.NEXT_PUBLIC_API_URL);
-console.log("Axios Base URL:", api.defaults.baseURL);
-
+let baseURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
+if (baseURL && !baseURL.endsWith('/api')) {
+  baseURL = baseURL.replace(/\/$/, '') + '/api';
+}
+const api = axios.create({ baseURL });
 export default api;
